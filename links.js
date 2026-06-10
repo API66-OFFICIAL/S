6e0df06fa6bd635b0276cd9a45b32da032db79f7
API66-OFFICIAL <bernardimport7@gmail.com>
2026-06-10 22:36:02 +0700

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
-const LINKS = {
+window.LINKS = {
   promo1: "https://google.com/",
   promo2: "https://youtube.com/",
   promo3: "https://facebook.com/",
-  promo4: "https://shopee.com/",
+  promo4: "https://shopee.com/"
 };
