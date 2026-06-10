be9acc98383b57bad844bd9c040057f968dae5cf
API66-OFFICIAL <bernardimport7@gmail.com>
2026-06-10 22:40:06 +0700

    Update links.js

--- links.js
+++ links.js
@@ -2,5 +2,11 @@ window.LINKS = {
   promo1: "https://google.com/",
   promo2: "https://youtube.com/",
   promo3: "https://facebook.com/",
-  promo4: "https://shopee.com/"
+  promo4: "https://shopee.com/",
+  promo5: "https://google.com/",
+  promo6: "https://youtube.com/",
+  promo7: "https://facebook.com/",
+  promo8: "https://shopee.com/"
+  promo9: "https://google.com/",
+  promo10: "https://youtube.com/"
 };
